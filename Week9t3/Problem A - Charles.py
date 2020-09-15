1a40a330621f15e747c4a43338c25213e91c4a2a
Charles <66225581+Quezard@users.noreply.github.com>
2020-09-16 11:27:29 +1200

    Update Problem A - Charles.py

--- Week9t3/Problem A - Charles.py
+++ Week9t3/Problem A - Charles.py
@@ -1,1 +1,12 @@
+book = input().split()
+pages = input().split()
+num = int(book[1])
+notebooks = 1
+for page in pages:
+  if int(page) > num:
+    notebooks += 1
+    num = 100 - int(page)
+  else:
+    num -= int(page)
 
+print(notebooks)
